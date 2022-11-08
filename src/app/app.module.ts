9922635118e6e578d7f39a934f605dfa6940e7a8
Jonathan William da Silva <jonathanwilliam.ws@gmail.com>
2022-11-08 17:16:53 -0300

    created news components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopComponent } from './top/top.component';
+import { PainelComponent } from './painel/painel.component';
+import { AttemptsComponent } from './attempts/attempts.component';
+import { ProgressComponent } from './progress/progress.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopComponent,
+    PainelComponent,
+    AttemptsComponent,
+    ProgressComponent
   ],
   imports: [
     BrowserModule
